e383194d6be7c456eabbec5af9be23d163475235
Wen Xiao <wennstyle@gmail.com>
2021-09-11 14:01:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function() {
+    return [drivers[0], drivers[1]];
+}
+
+const returnLastTwoDrivers = function() {
+    return [drivers[2],drivers[3]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    const fareMultiplier = function(fare){
+        return fare * multiplier;
+}
+}
